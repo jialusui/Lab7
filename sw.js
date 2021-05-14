e6e06957391a6ec85a5788179c530027218cd08f
jialusui <jisui@ucsd.edu>
2021-05-14 22:18:30 +0800

    update sw.js

--- sw.js
+++ sw.js
@@ -25,8 +25,8 @@
   self.addEventListener('activate', event => {
       event.waitUntil(clients.claim());
   });
-
-  self.addEventListener('fetch', function(event) {
+// https://cse110lab6.herokuapp.com/entries
+self.addEventListener('fetch', function(event) {
     event.respondWith(
       caches.match(event.request)
         .then(function(response) {
@@ -34,8 +34,34 @@
           if (response) {
             return response;
           }
-          return fetch(event.request);
-        }
-      )
-    );
+  
+          // IMPORTANT:Clone the request. A request is a stream and
+          // can only be consumed once. Since we are consuming this
+          // once by cache and once by the browser for fetch, we need
+          // to clone the response.
+          var fetchRequest = event.request.clone();
+  
+          return fetch(fetchRequest).then(
+            function(response) {
+              // Check if we received a valid response
+              if(!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+  
+              // IMPORTANT:Clone the response. A response is a stream
+              // and because we want the browser to consume the response
+              // as well as the cache consuming the response, we need
+              // to clone it so we have two streams.
+              var responseToCache = response.clone();
+  
+              caches.open(CACHE_NAME)
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+  
+              return response;
+            }
+          );
+        })
+      );
   });
